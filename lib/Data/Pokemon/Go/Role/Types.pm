35840bf545a0814540614419744d8a3ef62b3954
Yuki Yoshida <worthmine@gmail.com>
2018-09-20 23:18:16 +0900

    fixed back

--- lib/Data/Pokemon/Go/Role/Types.pm
+++ lib/Data/Pokemon/Go/Role/Types.pm
@@ -21,9 +21,9 @@ no Moose::Role;
 use Path::Tiny;
 use YAML::XS;
 
-use FindBin::libs qw( export );
-
-my $relation = path( $lib[0], '..', 'data', 'Relations.yaml' );
+#use FindBin::libs qw( export );
+#my $relation = path( $lib[0], '..', 'data', 'Relations.yaml' );
+my $relation = path( 'data', 'Relations.yaml' );
 our $Ref_Advantage = YAML::XS::LoadFile($relation);
 our $Relations = {};
 
