beefe916f705b7b4d4747358a8dd8518b2cda4a5
worthmine <worthmine@cpan.org>
2019-04-07 11:24:07 +0900

    test

--- share/MeCab.sh
+++ share/MeCab.sh
@@ -2,7 +2,8 @@
 
 
 if [ ! -e user.dic ]; then
-    /usr/local/bin/mecab/mecab-dict-index \
+    where mecab-dict-index
+    /usr/local/libexec/mecab/mecab-dict-index \
         -d /usr/local/lib/mecab/dic/mecab \
         -u user.dic -f UTF8 -t UTF8 MeCab.csv
 fi
