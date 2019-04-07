59dac4fa76b177e08c5518240bfcd171871f43dc
worthmine <worthmine@cpan.org>
2019-04-07 13:07:37 +0900

    gave up to run the tests around dictionary

--- share/MeCab.sh
+++ share/MeCab.sh
@@ -4,6 +4,6 @@
 if [ ! -e user.dic ]; then
     where mecab-dict-index
     /usr/local/libexec/mecab/mecab-dict-index \
-        -d /usr/local/lib/mecab/dic/mecab \
-        -u user.dic -f UTF8 -t UTF8 MeCab.csv
+        -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd \ # 環境依存
+        -u user.dic -f UTF8 -t UTF8 share/MeCab.csv
 fi
